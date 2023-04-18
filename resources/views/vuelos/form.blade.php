f582412fba6bb6ffdad245d260774de35568b8ac
Cesar Martinez <oliver-cesar@hotmail.com>
2023-04-18 10:24:26 -0600

    Table 1 Added Vuelos

---
+++ resources/views/vuelos/form.blade.php
@@ -0,0 +1,14 @@
+
+Numero de Vuelo:
+<input type="number" name="number_vuelo" id="number_vuelo" ">
+
+Origen:
+<input type="text" name="origen" id="origen" ">
+
+Destino:
+<input type="text" name="destino" id="destino" ">
+
+Fecha de Salida:
+<input type="date" name="fecha_salida" id="fecha_salida" ">
+
+<button type="submit">Save</button>
